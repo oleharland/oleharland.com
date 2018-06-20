a13b270f1a7fa375f732e640ff2b157058fe410c
Ole Harland <hello@oleharland.com>
2018-06-20 16:03:30 +0200

    repair deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
-gulp clean
-gulp
-rsync -r dist/ u80170328@home566746535.1and1-data.host:/kunden/homepages/43/d566746535/htdocs/oleharland
-=======
 echo "Starting in 5 seconds to 🚢"
 sleep 1
 echo "4"
